5bf5d7b662b634f9414b89fababa468aee2b676b
Ibso77 <ibrohimjonsobirov68@gmail.com>
2026-04-12 09:34:51 +0500

    .

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+// 1. Base URL ni aniqlash
 const baseURL = import.meta.env.DEV
   ? "/api"
   : import.meta.env.VITE_API_URL;
 
+// 2. Axios instance yaratish
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: baseURL, // Bu yerda yuqoridagi o'zgaruvchini ishlatish kerak
   headers: {
     "Content-Type": "application/json",
   },
